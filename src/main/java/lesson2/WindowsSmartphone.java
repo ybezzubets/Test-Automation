7387bc06d91a1e922dd960e856aae267550df652
ybezzubets <46883181+ybezzubets@users.noreply.github.com>
2019-02-12 10:58:22 +0200

    add class WindowsSmartphone

---
+++ src/main/java/lesson2/WindowsSmartphone.java
@@ -0,0 +1,42 @@
+package lesson2;
+
+public class WindowsSmartphone extends MobilePhones
+
+{
+	private int smartphoneVersionOS;
+	private String smartphoneBrand;
+	
+	public WindowsSmartphone(int i, String string) {
+	
+	}
+
+	public String getSmartphoneBrand() {
+		return smartphoneBrand;
+	}
+	
+	public void setSmartphoneBrand(String smartphoneBrand) {
+		this.smartphoneBrand = smartphoneBrand;
+	}
+	
+	public int getSmartphoneVersionOS() {
+		return smartphoneVersionOS;
+	}
+	
+	public void setSmartphoneVersionOS(int smartphoneVersionOS) {
+		this.smartphoneVersionOS = smartphoneVersionOS;
+	}
+	
+	public static void main(String[] args) {
+		WindowsSmartphone Nokia = new WindowsSmartphone(8, "Nokia");
+		Nokia.upgradeFirmware();
+		WindowsSmartphone Samsung = new WindowsSmartphone(7, "Samsung");
+		Samsung.upgradeFirmware();
+		WindowsSmartphone Siemens = new WindowsSmartphone(6, "Siemens");
+		Siemens.upgradeFirmware();
+		
+	}
+
+	public void upgradeFirmware() {
+		setSmartphoneVersionOS(getSmartphoneVersionOS() + 1);
+	}
+}
