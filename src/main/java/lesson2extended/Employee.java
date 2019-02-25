a3630b477a364a4448e2ac884180d1905d8786bb
yaroslav <yaroslav@y-bezubets-debian.kitsoft.kiev.ua>
2019-02-25 10:42:23 +0200

    add new class Room

--- src/main/java/lesson2extended/Employee.java
+++ src/main/java/lesson2extended/Employee.java
@@ -2,17 +2,22 @@ package lesson2extended;
 
 public class Employee extends Man{
 	private String position;
-	public Employee(String n, String s, String p){
-		name =n;
-		surname = s;
-		position =p;
-	}
-	
-	public void setPosition(String newProfession){
-		position = newProfession;
-	}
-	
-	public String getPosition(){
-		return position;
-	}
+    private IdCard iCard;
+    public Employee(String n, String s, String p){
+        name = n;
+        surname = s;
+        position = p;
+    }
+    public void setPosition(String newPosition){
+        position = newPosition;
+    }
+    public String getPosition(){
+        return position;
+    }
+    public void setIdCard(IdCard c){
+        iCard = c;
+    }
+    public IdCard getIdCard(){
+        return iCard;
+    }
 }
---
+++ src/main/java/lesson2extended/Room.java
@@ -0,0 +1,14 @@
+package lesson2extended;
+
+public class Room {
+	private int number;
+    public Room(int n){
+        number = n;
+    }
+    public void setNumber(int newNumber){
+        number = newNumber;
+    }
+    public int getNumber(){
+        return number;
+    }
+}
