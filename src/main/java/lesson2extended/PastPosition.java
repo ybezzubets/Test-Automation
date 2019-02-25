666a5314dc5aee2379071e59abdc3442f3e94c5a
yaroslav <yaroslav@y-bezubets-debian.kitsoft.kiev.ua>
2019-02-25 12:32:32 +0200

    add new class PastPosition

---
+++ src/main/java/lesson2extended/PastPosition.java
@@ -0,0 +1,22 @@
+package lesson2extended;
+
+public class PastPosition{
+    private String name;
+     private Department department;
+     public PastPosition(String position, Department dep){
+         name = position;
+         department = dep;
+     }
+     public void setName(String newName){
+         name = newName;
+     }
+     public String getName(){
+         return name;
+     }
+     public void setDepartment(Department d){
+         department = d;
+     }
+     public Department getDepartment(){
+         return department;
+     }
+}
