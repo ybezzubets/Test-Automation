8606af2a48e45ea7e0be067dc1471cf32dabc5fe
yaroslav <yaroslav@y-bezubets-debian.kitsoft.kiev.ua>
2019-02-25 10:23:12 +0200

    add employee class

--- src/main/java/lesson2extended/Employee.java
+++ src/main/java/lesson2extended/Employee.java
@@ -1,5 +1,18 @@
 package lesson2extended;
 
-public class Employee {
-
+public class Employee extends Man{
+	private String position;
+	public Employee(String n, String s, String p){
+		name =n;
+		surname = s;
+		position =p;
+	}
+	
+	public void setPosition(String newProfession){
+		position = newProfession;
+	}
+	
+	public String getPosition(){
+		return position;
+	}
 }
